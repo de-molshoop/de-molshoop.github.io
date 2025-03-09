cd2130b60f99ad97ab225e8a352d0a075ea33e0c
xWishky <wies.van.lommel@wimsoft.be>
2025-03-09 14:39:40 +0100

    Base of the quiz page done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/Login";
 import HomePage from "./pages/Home";
 import AuthCallback from "./pages/AuthCallback";
+import QuizInputPage from "./pages/Quiz";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
                 <Route path="/" element={<HomePage />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/auth/callback" element={<AuthCallback/>} />
+                <Route path="/quiz" element={<QuizInputPage/>}></Route>
             </Routes>
         </Router>
     );
